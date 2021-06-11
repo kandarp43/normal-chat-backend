0039ff71f0341c1b3464b5dfa9e60cfcb3032384
kandarp dangi <kandarpdangi@gmail.com>
2021-06-11 19:32:11 +0530

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const httpServer = require('http').createServer()
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST'],
+  },
+})
+
+httpServer.listen(5000, () => {
+  console.log('running')
+})
+io.on('connection', (socket) => {
+  const id = socket.handshake.query.id
+  socket.join(id)
+  socket.on('send-message', ({ recipients, text }) => {
+    recipients.forEach((recipient) => {
+      const newRecipients = recipients.filter((r) => r !== recipient)
+      newRecipients.push(id)
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients,
+        sender: id,
+        text,
+      })
+    })
+  })
+})
